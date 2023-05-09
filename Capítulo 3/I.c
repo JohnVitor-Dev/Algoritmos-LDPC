9ea392ab1b359a6fd1d98a5c6648219f37b53419
John Vitor <johnvitoralves39@gmail.com>
2023-05-09 15:59:33 -0300

    Create I.c

---
+++ Capítulo 3/I.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, R;
+    
+    printf("Digite o valor de N: \n");
+    scanf("%d", &N);
+    
+    R = N * N; 
+    
+    printf("Valor de N²: %d \n", R);
+
+    return 0;
+}
