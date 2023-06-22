59f8cad9a6d9d9abbbf8be9055f2e71cff0a640b
John Vitor <johnvitoralves39@gmail.com>
2023-06-22 04:54:11 -0300

    Create N.c

---
+++ Capítulo 4/N.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    
+    int A, B, C;
+    int soma;
+
+    printf("Digite o valor de A: ");
+    scanf("%d", &A);
+
+    printf("Digite o valor de B: ");
+    scanf("%d", &B);
+
+    printf("Digite o valor de C: ");
+    scanf("%d", &C);
+
+    soma = A + B + C;
+
+    if (soma >= 100) {
+        
+        printf("Soma = %d\n", soma);
+    }
+
+    return 0;
+}
